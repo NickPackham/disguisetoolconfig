77aac27060603d94fd9ca642a22d2ba2174cb8c0
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-07 19:26:27 -0700

    Create config.lua

---
+++ lua/voy_config_disguise/config.lua
@@ -0,0 +1,20 @@
+VOY_GM_Disguise = {}
+
+VOY_GM_Disguise_Main = {
+	["1"] = { -- Add one to this every time you add a new table.
+		VOY_Disguise_Name = "Mossman", -- Name of it that will be identified in the Dropdown.
+		VOY_Disguise_Model = "models/mossman.mdl", -- Model of the Disguise.
+		VOY_Disguise_Health = 20, -- Health
+		VOY_Disguise_MaxHealth = 20, -- Max Health (Meant for people who use the shitty Medical supplies that reset health.)
+		VOY_Disguise_Armor = 20, -- Armor
+		VOY_Disguise_Weapons = {"weapon_ak472"}, -- Weapons
+	},
+	["2"] = { -- Add one to this every time you add a new table.
+		VOY_Disguise_Name = "Kleiner", -- Name of it that will be identified in the Dropdown.
+		VOY_Disguise_Model = "models/Kleiner.mdl", -- Model of the Disguise.
+		VOY_Disguise_Health = 20, -- Health
+		VOY_Disguise_MaxHealth = 20, -- Max Health (Meant for people who use the shitty Medical supplies that reset health.)
+		VOY_Disguise_Armor = 20, -- Armor
+		VOY_Disguise_Weapons = {"weapon_ak472"}, -- Weapons
+	},
+}
